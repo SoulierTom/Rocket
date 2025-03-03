84c76855c9a5e31b86bd2d83b12e8b5a04a47041
Mayeul <mayeul.ferrand@brassart.fr>
2025-03-03 10:37:22 +0100

    animation de mort

--- Scripts/kill.gd
+++ Scripts/kill.gd
@@ -1,5 +1,20 @@
 extends Area2D
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"): # Assure-toi que ton personnage est dans le groupe "Player"
+	if body.is_in_group("Player"):
+		print("Player entered death area")
+		if is_instance_valid(DeathScreen):
+			print("DeathScreen is valid, playing death animation")
+			DeathScreen.death()
+			await DeathScreen.on_death_finished
+			print("Death animation finished, reloading scene")
+			call_deferred("reload_scene")
+		else:
+			print("Error: DeathScreen is not valid.")
+
+func reload_scene():
+	if get_tree():
+		print("Reloading current scene")
 		get_tree().reload_current_scene()
+	else:
+		print("Error: Scene tree is null. Cannot reload scene.")
