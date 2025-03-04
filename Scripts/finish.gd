274e021752ab3636fed7c528c2b709f365007c57
Mayeul <mayeul.ferrand@brassart.fr>
2025-03-04 18:15:49 +0100

    oui je suis le goat

--- Scripts/finish.gd
+++ Scripts/finish.gd
@@ -7,9 +7,10 @@ extends Area2D
 @export var player_group: String = "Player"
 
 func _on_body_entered(body):
-	# Vérifie si l'objet en collision appartient au groupe "Player"
-	if body.is_in_group(player_group):
-		# Arrêter le Timer de speedrun
+	if body.is_in_group("Player"):
+		print("Player entered death area")
+		body.set_process(false)  # Désactive _process du joueur
+		body.set_physics_process(false)  # Désactive _physics_process du joueur
 		var speedrun_timer = get_tree().root.get_node("Speedrun") # Nom du nœud Timer
 		if speedrun_timer:
 			speedrun_timer.set_process(false) # Arrête le calcul dans le Timer
@@ -18,4 +19,6 @@ func _on_body_entered(body):
 
 		# Changer de scène
 		print("Chargement de la scène :", target_scene)
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
 		get_tree().change_scene_to_file(target_scene)
