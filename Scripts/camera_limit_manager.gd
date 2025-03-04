a1ba1d44439613802d407555c26fb6b3dcbeea62
Steldum <oswald.spielmann07@gmail.com>
2025-03-04 14:44:34 +0100

    Ld Autotile et limit manager

--- Scripts/camera_limit_manager.gd
+++ Scripts/camera_limit_manager.gd
@@ -5,7 +5,7 @@ class_name CameraLimitManager
 @onready var camera = $".."
 @onready var player = $"."
 
-@export var limit_transition_speed = 50
+@export var limit_transition_speed = 1000
 
 const MAX_LIMIT = 100000
 
