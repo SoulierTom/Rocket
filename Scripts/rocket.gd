7d1e9c60012ea5c4562a692571ae3eb5fa7743d0
Mayeul <mayeul.ferrand@brassart.fr>
2025-03-29 20:53:50 +0100

    rocket collision ok

--- Scripts/rocket.gd
+++ Scripts/rocket.gd
@@ -7,25 +7,17 @@ var direction = Vector2.ZERO
 @onready var existence: Timer = $Existence
 
 func _ready():
-	
 	set_as_top_level(true)
 	look_at(position + direction)
-	
-	#désactive le projectile au bout d'un certain temps
 	existence.timeout.connect(queue_free)
 
 func _physics_process(delta: float) -> void:
-	
-	position += direction * speed * delta
-	
-	# Déplacement et gestion des collisions
+	# Utilisez seulement move_and_collide avec la bonne vitesse
 	var collision = move_and_collide(direction * speed * delta)
 	
 	if collision:
-		explosion_notifier = get_parent()
 		# Notifie le gestionnaire de créer une explosion
+		explosion_notifier = get_parent()
 		if explosion_notifier:
 			explosion_notifier.call_deferred("create_explosion", global_position)
-		
 		queue_free()
-		
