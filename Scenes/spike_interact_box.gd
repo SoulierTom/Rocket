0508a1183a84e6dd7a27247c0b06de5b8cbae35d
AlgymerentGraou <andreitetrel@gmail.com>
2025-05-04 19:44:04 +0200

    Working Spikes
    + Git-Ignore LDtk levels

---
+++ Scenes/spike_interact_box.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+
+func _on_body_entered(body) -> void:
+	if body is TileMapLayer:
+		print("Player entered death area")
+		body.set_process(false)  # Désactive _process du joueur
+		body.set_physics_process(false)  # Désactive _physics_process du joueur
+		
+		if is_instance_valid(DeathScreen):
+			print("DeathScreen is valid, playing death animation")
+			DeathScreen.death()
+			await DeathScreen.on_death_finished
+			print("Death animation finished, reloading scene")
+			call_deferred("reload_scene")
+		else:
+			print("Error: DeathScreen is not valid.")
+
+func reload_scene():
+	if get_tree():
+		print("Reloading current scene")
+		get_tree().reload_current_scene()
+	else:
+		print("Error: Scene tree is null. Cannot reload scene.")
