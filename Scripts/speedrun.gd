958f487d5008ec192dcf183f894ef9aeb6d00561
Mayeul <mayeul.ferrand@brassart.fr>
2025-06-24 11:17:33 +0200

    timer update

--- Scripts/speedrun.gd
+++ Scripts/speedrun.gd
@@ -10,6 +10,7 @@ var blink_duration: float = 0.8
 var blink_interval: float = 0.1
 
 @onready var label: Label = $CanvasLayer/Label
+@onready var centieme_label: Label = $CanvasLayer/centieme
 
 func _ready():
 	add_to_group("speedrun_timer")
@@ -21,29 +22,24 @@ func _ready():
 		time = Global.speedrun_time
 
 func _physics_process(delta):
-	# Le timer continue TOUJOURS de tourner
 	time += delta
 	Global.speedrun_time = time
 	
-	# Gestion du clignotement
 	if is_blinking:
 		handle_blinking(delta)
 	
-	# Mise à jour de l'affichage
 	update_ui()
 
 func handle_blinking(delta: float):
-	"""Gère le clignotement pendant la transition"""
 	blink_timer += delta
-	
-	# Calculer si le label doit être visible ou invisible
 	var cycle_time = fmod(blink_timer, blink_interval * 2)
 	var should_be_visible = cycle_time < blink_interval
 	
 	if label:
 		label.visible = should_be_visible
+	if centieme_label:
+		centieme_label.visible = should_be_visible
 	
-	# Arrêter le clignotement après 0.8 secondes
 	if blink_timer >= blink_duration:
 		stop_blinking()
 
@@ -56,43 +52,42 @@ func reset_timer():
 	blink_timer = 0.0
 	if label:
 		label.visible = true
+	if centieme_label:
+		centieme_label.visible = true
 	update_ui()
 
 func freeze_display():
-	"""Fige l'affichage sur le temps actuel"""
 	display_frozen = true
 	frozen_time = time
 
 func unfreeze_display():
-	"""Remet l'affichage en temps réel"""
 	display_frozen = false
 
 func start_blinking():
-	"""Démarre le clignotement"""
 	is_blinking = true
 	blink_timer = 0.0
 
 func stop_blinking():
-	"""Arrête le clignotement"""
 	is_blinking = false
 	blink_timer = 0.0
 	if label:
 		label.visible = true
+	if centieme_label:
+		centieme_label.visible = true
 
 func update_ui():
-	if not label:
+	if not label or not centieme_label:
 		return
 	
-	# Choisir quel temps afficher
 	var display_time = frozen_time if display_frozen else time
 	
-	# Formatage du temps
 	var minutes = int(display_time) / 60
 	var seconds = int(display_time) % 60
 	var centiseconds = int((display_time - int(display_time)) * 100)
-	label.text = "%02d:%02d.%02d" % [minutes, seconds, centiseconds]
+	
+	label.text = "%02d:%02d" % [minutes, seconds]
+	centieme_label.text = ".%02d" % centiseconds
 
-# Fonction appelée depuis l'extérieur pour figer l'affichage et faire clignoter
 func trigger_level_complete():
 	freeze_display()
 	start_blinking()
